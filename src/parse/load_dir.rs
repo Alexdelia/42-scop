8bf9018eded254f396db5be614ad45742ff85908
Alexdelia <alexandre.delille.57@gmail.com>
2023-04-03 12:09:58 +0200

    group_file

--- src/parse/load_dir.rs
+++ src/parse/load_dir.rs
@@ -1,6 +1,7 @@
 use std::path::Path;
 
-pub fn load_dir(path: &Path) -> Result<Vec<std::path::PathBuf>, std::io::Error> {
+pub fn load_dir(dir: impl AsRef<Path>) -> Result<Vec<std::path::PathBuf>, std::io::Error> {
+    let path = dir.as_ref();
     Ok(std::fs::read_dir(path)
         .map_err(|e| {
             eprintln!("failed to read directory '{}'\n{e}", path.display());
--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -1,6 +1,4 @@
 mod load_dir;
-use std::path::PathBuf;
-
 pub use load_dir::load_dir;
 mod obj;
 
@@ -9,13 +7,17 @@ use yahmrslib::hmerr::Result;
 use crate::setting::OBJ_PATH;
 use crate::Object;
 
+use std::path::{Path, PathBuf};
+
 pub fn parse() -> Result<Vec<Object>> {
     let dir = group_file(load_dir(OBJ_PATH)?);
 
-    if obj.is_empty() {
-        Err("did not manage to load any object")
+    if dir.is_empty() {
+        todo!("return error: did not manage to load any object");
+        // Err("did not manage to load any object")
     } else {
-        Ok(obj)
+        todo!("return Ok(dir)");
+        // Ok(dir)
     }
 }
 
@@ -24,25 +26,82 @@ fn group_file(dir: Vec<PathBuf>) -> Vec<(PathBuf, Option<PathBuf>)> {
 
     // check if each file.obj has his file.mtl
     for obj in dir {
-		if  && ext != "obj" && ext != "mtl" {
-			eprintln!("WARNING with {}: only treating .obj/.mtl files", obj.display());
-			continue;
-		};
-		if ext != "obj" && ext != "mtl" {
-
-
-        let mut mtl = None;
-        if let Some(ext) = obj.extension() {
-            if ext == "obj" {
-                let mut mtl_path = obj.clone();
-                mtl_path.set_extension("mtl");
-                if mtl_path.exists() {
-                    mtl = Some(mtl_path);
+        match check_ext(&obj) {
+            Ext::None => {
+                eprintln!(
+                    "WARNING with {}: only treating .obj/.mtl files",
+                    obj.display()
+                );
+                continue;
+            }
+            Ext::Mtl => continue,
+            Ext::Obj => {
+                let mut mtl = obj.clone();
+                mtl.set_extension("mtl");
+                if mtl.exists() {
+                    ret.push((obj, Some(mtl)));
+                } else {
+                    ret.push((obj, None));
                 }
             }
         }
-        ret.push((obj, mtl));
     }
 
     ret
+}
+
+enum Ext {
+    Obj,
+    Mtl,
+    None,
+}
+
+fn check_ext(path: &Path) -> Ext {
+    match path.extension() {
+        Some(ext) if ext == "obj" => Ext::Obj,
+        Some(ext) if ext == "mtl" => Ext::Mtl,
+        _ => Ext::None,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::path::Path;
+
+    // derive PartialEq for Ext
+    impl PartialEq for Ext {
+        fn eq(&self, other: &Self) -> bool {
+            match (self, other) {
+                (Ext::Obj, Ext::Obj) => true,
+                (Ext::Mtl, Ext::Mtl) => true,
+                (Ext::None, Ext::None) => true,
+                _ => false,
+            }
+        }
+    }
+    impl std::fmt::Debug for Ext {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            match self {
+                Ext::Obj => write!(f, "Ext::Obj"),
+                Ext::Mtl => write!(f, "Ext::Mtl"),
+                Ext::None => write!(f, "Ext::None"),
+            }
+        }
+    }
+
+    #[test]
+    fn test_check_ext() {
+        let path = Path::new("test.obj");
+        assert_eq!(check_ext(path), Ext::Obj);
+
+        let path = Path::new("test.mtl");
+        assert_eq!(check_ext(path), Ext::Mtl);
+
+        let path = Path::new("test.txt");
+        assert_eq!(check_ext(path), Ext::None);
+
+        let path = Path::new("test");
+        assert_eq!(check_ext(path), Ext::None);
+    }
 }
