68f9e61b8a69925bbfe1fa24d37d163fecff45c6
Alexdelia <alexandre.delille.57@gmail.com>
2023-04-11 18:37:03 +0200

    finished parse::line

--- src/parse/line/mod.rs
+++ src/parse/line/mod.rs
@@ -1,5 +1,3 @@
-mod get_line;
-pub use get_line::get_line;
 mod keyword;
 pub use keyword::Keyword;
 mod format;
@@ -12,6 +10,27 @@ use crate::prelude::*;
 use crate::parse::FileData;
 
 use ansi::abbrev::{B, D, G, M, R, Y};
+
+type ParsedLine = (Vec<String>, usize); // (tokens, line_index)
+
+pub fn get_line(f: &FileData, el: &ExpectedLine) -> Result<Vec<ParsedLine>> {
+    let mut ret: Vec<ParsedLine> = Vec::new();
+
+    for i in 0..f.diluted.len() {
+        if f.diluted[i].starts_with(el.k.keyword) {
+            ret.push((el.check(f, i)?, i));
+        }
+    }
+
+    match el.occurence.check(ret.len()) {
+        Ok(_) => Ok(ret),
+        Err(e) => Err(pfe!(
+            f!("{B}{Y}{}{D} {e}", el.k.keyword),
+            h:el.help(),
+            f:f.name,
+        ))?,
+    }
+}
 
 pub struct ExpectedLine {
     pub k: Keyword,
@@ -30,22 +49,28 @@ impl ExpectedLine {
 
     pub fn check(&self, f: &FileData, line_index: usize) -> Result<Vec<String>> {
         let line = f.diluted[line_index].replacen(self.k.keyword, "", 1).trim();
-        let token = self.format.check(line).map_err(|(expected, got)| {
+        match self.format.check(line).map_err(|(expected, got)| {
             pfe!(
                 f!("expected {B}{G}{expected}{D} token after {B}{Y}{keyword}{D}, got {B}{R}{got}{D}",
 					keyword=self.k.keyword,
 			),
-                h:f!(
-"{B}{Y}{keyword}{D} define {desc}
-the line must follow the format: `{B}{keyword} {M}{format}{D}`",
-					keyword=self.k.keyword,
-					desc=self.k.desc,
-					format=self.format.token,
-				),
+                h:self.help(),
                 f:f.name,
                 l:ple!(line, i:line_index, w:pwe!((0, line.len())))
             )
-        })?;
-        Ok(token)
+        }) {
+			Ok(token) => Ok(token),
+			Err(e) => Err(e)?,
+		}
+    }
+
+    pub fn help(&self) -> String {
+        f!(
+            "{B}{Y}{keyword}{D} define {desc}
+the line must follow the format: `{B}{keyword} {M}{format}{D}`",
+            keyword = self.k.keyword,
+            desc = self.k.desc,
+            format = self.format.token,
+        )
     }
 }
