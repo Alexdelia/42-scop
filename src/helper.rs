04fa94e13bbad90e4eec8b72bad17c39eb94c649
adelille <adelille@student.42.fr>
2023-09-29 11:49:28 +0200

    save

--- src/helper.rs
+++ src/helper.rs
@@ -1,3 +1,8 @@
+use ansi::{
+    abbrev::{B, G},
+    ASCII_42, ASCII_COLORED_THE_ORDER, COLOR_42, COLOR_42_INTRA,
+};
+
 pub fn help() {
-    println!("HELP");
+    println!("{B}{COLOR_42}{ASCII_42}\tscop\n{ASCII_COLORED_THE_ORDER}");
 }
